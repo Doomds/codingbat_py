85fd153efd4a3756fa1b8372e233d500e4e1cfe4
doom <de.simone.dc@gmail.com>
2025-08-13 16:21:14 +0200

    Ajout des tests dans string_times et ajout de front_times

---
+++ Warmup-2/front_times.py
@@ -0,0 +1,7 @@
+def front_times(str, n):
+  return str[:3] * n
+
+# Tests
+print(front_times('Chocolate', 2))
+print(front_times('Chocolate', 3))
+print(front_times('Abc', 3))
--- Warmup-2/string_times.py
+++ Warmup-2/string_times.py
@@ -6,3 +6,10 @@ def string_times(str, n):
     new_str = str * n
         
   return new_str
+
+
+# Tests
+print(string_times('Hi', 2))
+print(string_times('Hi', 3))
+print(string_times('Hi', 3))
+print(string_times('Hi', 0))
