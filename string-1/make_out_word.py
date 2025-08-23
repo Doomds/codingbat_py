deff9ab5fd61241846afbb55465469bdaefa47f9
doom <de.simone.dc@gmail.com>
2025-08-23 16:10:49 +0200

    add solution make_out_word

---
+++ string-1/make_out_word.py
@@ -0,0 +1,8 @@
+def make_out_word(out, word):
+    return out[:2] + word + out[-2:]
+
+
+# Tests
+print(make_out_word('<<>>', 'Yay')) # '<<Yay>>'
+print(make_out_word('<<>>', 'WooHoo')) # '<<WooHoo>>'
+print(make_out_word('[[]]', 'word')) # '[[word]]'
