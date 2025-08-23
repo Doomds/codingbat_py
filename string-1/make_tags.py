88d926142c97c43075da0d94fd518acf61c7c9dd
doom <de.simone.dc@gmail.com>
2025-08-23 11:51:05 +0200

    ajout des solution hello_name, make_abba, make_tags

---
+++ string-1/make_tags.py
@@ -0,0 +1,8 @@
+def make_tags(tag, word):
+    return f"<{tag}>{word}</{tag}>"
+
+
+# Tests
+print(make_tags('i', 'Yay')) # '<i>Yay</i>'
+print(make_tags('i', 'Hello')) # '<i>Hello</i>'
+print(make_tags('cite', 'Yay')) # '<cite>Yay</cite>'
