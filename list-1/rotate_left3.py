a724e45734324a3c0fed6f307cee98e147e277c7
doom <de.simone.dc@gmail.com>
2025-09-13 10:29:15 +0200

    ajout de typage

--- list-1/rotate_left3.py
+++ list-1/rotate_left3.py
@@ -1,8 +1,9 @@
-def rotate_left3(nums):
+def rotate_left3(nums: list[int]):
     nums.append(nums[0])
     nums.remove(nums[0])
     return nums
 
-print(rotate_left3([1, 2, 3])) # [2, 3, 1]
-print(rotate_left3([5, 11, 9])) # [11, 9, 5]
-print(rotate_left3([7, 0, 0])) # [0, 0, 7]
+
+print(rotate_left3([1, 2, 3]))  # [2, 3, 1]
+print(rotate_left3([5, 11, 9]))  # [11, 9, 5]
+print(rotate_left3([7, 0, 0]))  # [0, 0, 7]
